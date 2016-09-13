009826c429306cb6141b75a4cbc908fcceabde60
Joel Eriksson <joel.eriksson3@itggot.se>
2016-09-13 19:00:08 +0200

    Changes for responsive website

--- js/main.js
+++ js/main.js
@@ -1,3 +1,20 @@
 $(function() {
+    /* Will close the navigation if the window width goes below 1100px */
+    $(window).resize(function(event) {
+        if ($(this).width < 1100) {
+            $(".navbar__container").removeClass("navbar__container--open").addClass("navbar__container--closed");
+        }
+    });
 
+    /* For opening the navigation */
+    $(".navbar__trigger").on("click", function(event) {
+        event.preventDefault();
+        $(".navbar__container").removeClass("navbar__container--closed").addClass("navbar__container--open");
+    });
+
+    /* For closing the navigation */
+    $(".navbar__item--cta-button").on("click", function(event) {
+        event.preventDefault();
+        $(".navbar__container").removeClass("navbar__container--open").addClass("navbar__container--closed");
+    });
 });
